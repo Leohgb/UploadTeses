82254cec1b681af6866f077f87bca645eafc2f13
Leonardo  Leal <91156801+Leohgb@users.noreply.github.com>
2022-12-29 15:35:18 -0300

    Update nodemailer.config.js

--- config/nodemailer.config.js
+++ config/nodemailer.config.js
@@ -26,7 +26,7 @@ module.exports.sendConfirmationEmail = (name, email, confirmacao) => {
           <h2>Olá ${name}</h2>
           <p>Obrigado por criar uma conta. Por favor confirme seu Email clicando no link abaixo</p>
           <form action="https://uploadteses.onrender.com/usuarios/registro/${confirmacao}" method="POST">
-          <input type='submit' style="margin-top:5px; border-color:#17a2b8; border: 1px solid transparent; background-color:#17a2b8; color:#fff; border-radius: 0.25rem; font-size: 1rem;
+          <input type='submit' style="margin-top:5px; cursor: pointer; ;border-color:#17a2b8; border: 1px solid transparent; background-color:#17a2b8; color:#fff; border-radius: 0.25rem; font-size: 1rem;
           transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
           padding: 0.375rem 0.75rem;"
           value='Confirme' />
